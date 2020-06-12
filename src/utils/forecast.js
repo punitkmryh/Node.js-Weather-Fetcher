1f943e00708a6f902b9e436939a654162f6806cc
Punitkumar Y Harsoor <39958570+punitkmryh@users.noreply.github.com>
2020-06-12 14:27:54 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,11 +18,11 @@ const forecast = (latitude, longitude, callback) => {
         body.daily.data[0].summary +
           " It is currently " +
           body.currently.temperature +
-          " degress out. This high today is " +
+          " ° out. With high today is " +
           body.daily.data[0].temperatureHigh +
-          " with a low of " +
+          "° with a low of " +
           body.daily.data[0].temperatureLow +
-          ". There is a " +
+          "°. There is a " +
           body.currently.precipProbability +
           "% chance of rain."
       );
