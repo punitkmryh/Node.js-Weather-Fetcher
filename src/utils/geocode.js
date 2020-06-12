879fe58f3354d446be59ecd397daa513ff4d59e9
Punitkumar Y Harsoor <39958570+punitkmryh@users.noreply.github.com>
2020-06-12 14:29:10 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
     encodeURIComponent(address) +
-    ".json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1";
+    ".json?access_token=pk.eyJ1IjoicHVuaXRrbXIiLCJhIjoiY2s2a2lyMGltMDRnMjNlcGJvMjNmeDU3YyJ9.4utrPICKCxkBUIzZDR6FE&limit=1";
 
   request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
